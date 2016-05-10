0d8655a3bc07c6f127c982296042ee95ee09b106
Matt Calthrop <mattcalthrop@Matts-MacBook-Pro.local>
2016-05-10 15:16:10 +0100

    Simplify examples; add --no-treeshake param

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-// assign a value to the `window` variable, which
-window.greeting = 'Merry';
 // declare an array of numbers for later use
 var numbers = [100, 200, 300];
+// declare an array of scores, initially empty
+var scores = [];
 // declare an object that will be bound in order to provide context
 var context = {
-    greeting: 'Happy'
+    scores: []
 };
-
-function appendStringToThisGreeting(stringToAppend) {
-    console.log('appendStringToThisGreeting(): this:', this);
-    this.greeting += stringToAppend;
-    console.log('appendStringToThisGreeting(): after append: this.greeting:', this.greeting, '; context.greeting:', context.greeting);
-}
